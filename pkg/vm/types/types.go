0efe241f3ec38f2b404410522e5f0ab6de4d825a
Rak Laptudirm <raklaptudirm@gmail.com>
2022-02-01 21:43:02 +0530

    chore: clean slate

--- pkg/vm/types/types.go
+++
@@ -1,8 +0,0 @@
-/*
-Package types provides data used by the vm.
-*/
-package types
-
-// TAPE_LENGTH represents the number of memory cells
-// present in the VM struct's memory.
-const TAPE_LENGTH uint16 = 30000
