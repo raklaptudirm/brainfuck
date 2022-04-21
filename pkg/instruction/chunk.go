d0cbcf801cdf0fafe5d4a7f62236cf78140c9230
Rak Laptudirm <raklaptudirm@gmail.com>
2022-04-21 22:43:07 +0530

    chore: create `Chunk` type

---
+++ pkg/instruction/chunk.go
@@ -0,0 +1,49 @@
+// Copyright © 2022 Rak Laptudirm <raklaptudirm@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package instruction
+
+import "fmt"
+
+// NewChunk creates a new Chunk with the given instruction list.
+func NewChunk(ins []Instruction) *Chunk {
+	return &Chunk{ins: ins}
+}
+
+// Chunk represents an immutable list of instructions.
+type Chunk struct {
+	ins []Instruction
+}
+
+// String converts a Chunk into a human readable string.
+func (c *Chunk) String() string {
+	var s string
+	length := c.Len()
+
+	for i := 0; i < length; i++ {
+		s += fmt.Sprintf("%4d %s\n", i, c.Instruction(i).Instruction())
+	}
+
+	return s
+}
+
+// Instruction fetches the ith instruction in the Chunk. It will panic if i
+// is greater than the length of the Chunk.
+func (c *Chunk) Instruction(i int) Instruction {
+	return c.ins[i]
+}
+
+// Len returns the length of the Chunk.
+func (c *Chunk) Len() int {
+	return len(c.ins)
+}
--- pkg/instruction/instruction.go
+++ pkg/instruction/instruction.go
@@ -17,9 +17,11 @@
 // target.
 package instruction
 
+import "fmt"
+
 // Instruction represents a brainfuck instruction.
 type Instruction interface {
-	Instruction()
+	Instruction() string
 }
 
 // Value instruction changes the value of the current cell by X.
@@ -27,36 +29,50 @@ type Value struct {
 	X byte
 }
 
-func (v *Value) Instruction() {}
+func (v *Value) Instruction() string {
+	return fmt.Sprintf("%-16s %3d", "CHANGE_VAL", v.X)
+}
 
 // Pointer instruction changes the value pointer by X.
 type Pointer struct {
 	X int
 }
 
-func (p *Pointer) Instruction() {}
+func (p *Pointer) Instruction() string {
+	return fmt.Sprintf("%-16s %d", "CHANGE_PTR", p.X)
+}
 
 // Input instruction takes a single byte as input from the user.
 type Input struct{}
 
-func (i *Input) Instruction() {}
+func (i *Input) Instruction() string {
+	return "INPUT"
+}
 
 // Output instruction outputs the value of the current cell.
 type Output struct{}
 
-func (o *Output) Instruction() {}
+func (o *Output) Instruction() string {
+	return "OUTPUT"
+}
 
 // StartLoop instruction signals the start of a loop.
 type StartLoop struct{}
 
-func (s *StartLoop) Instruction() {}
+func (s *StartLoop) Instruction() string {
+	return "START_LOOP"
+}
 
 // EndLoop instruction signals the end of a loop.
 type EndLoop struct{}
 
-func (e *EndLoop) Instruction() {}
+func (e *EndLoop) Instruction() string {
+	return "END_LOOP"
+}
 
 // Clear sets the current cell's value to 0.
 type Clear struct{}
 
-func (c *Clear) Instruction() {}
+func (c *Clear) Instruction() string {
+	return "CLEAR"
+}
