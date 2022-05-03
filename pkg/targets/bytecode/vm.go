c5f7d1429c70e0ad0438ac9be538d7e8587182e2
Rak Laptudirm <raklaptudirm@gmail.com>
2022-05-03 17:56:15 +0530

    feat: buffer output for better performance

--- pkg/targets/bytecode/vm.go
+++ pkg/targets/bytecode/vm.go
@@ -15,6 +15,7 @@ package bytecode
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -29,8 +30,16 @@ type vm struct {
 // an unknown instruction. Currently the memory length of the vm is hard-
 // coded to 30000.
 func Run(c []byte) {
+	// TODO: customizable memory length
 	v := vm{memory: make([]byte, 30000)}
 	length := len(c)
+
+	// TODO: make this customizable
+	buffer := printBuffer{
+		writer: os.Stdout,
+		aFlush: true,
+		length: 50,
+	}
 
 	for i := 0; i < length; i++ {
 		switch Bytecode(c[i]) {
@@ -46,7 +55,8 @@ func Run(c []byte) {
 		case InputByte:
 			fmt.Scanf("%c", &v.memory[v.pointer])
 		case OutputByte:
-			os.Stdout.Write([]byte{v.memory[v.pointer]})
+			// syscalls are expensive, buffer output bytes
+			buffer.Write(v.memory[v.pointer])
 
 		case JumpIfZero:
 			x, l := uintFromBytes(c[i+1:])
@@ -74,4 +84,39 @@ func Run(c []byte) {
 			panic(fmt.Sprintf("vm: invalid bytecode instruction %2x", c[i]))
 		}
 	}
+
+	// flush any remaining bytes
+	buffer.Flush()
+}
+
+// printBuffer is a helper struct which buffers byte outputs for better
+// performance, as syscalls are expensive.
+type printBuffer struct {
+	buffer []byte // backlog
+
+	// options
+	writer io.Writer // writer to output to
+	aFlush bool      // automatically flush at intervals
+	length int       // max backlog, only applicable if aFlush = true
+}
+
+// Write puts the given bytes into the backlog, and flushes it if it's
+// length exceeds the provided maximum, and aFlush = true.
+func (b *printBuffer) Write(bytes ...byte) {
+	b.buffer = append(b.buffer, bytes...)
+
+	if b.aFlush && len(b.buffer) > b.length {
+		b.Flush()
+	}
+}
+
+// Flush empties the backlog into the writer.
+func (b *printBuffer) Flush() {
+	// check if backlog is empty
+	if len(b.buffer) == 0 {
+		return
+	}
+
+	b.writer.Write(b.buffer)
+	b.buffer = []byte{}
 }
